6a06ebd5b76f478a70df37041d22481a37b8e352
Joel-Galvan <joelgv1003@gmail.com>
2024-09-06 16:26:08 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -15,7 +15,7 @@
 //     }
 // }
 import { CorsOptions } from "cors"
-export const corsOptions: CorsOptions = {
+export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         if (origin === process.env.FRONTEND_URL) {
             callback(null, true)
