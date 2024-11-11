21d0a90ae5d5f4b289444e51ab7e2a8f66088634
Joel-Galvan <joelgv1003@gmail.com>
2024-11-11 18:16:58 -0500

    Ultimo punto

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,30 @@
-import { CorsOptions } from 'cors'
-//Cors
+// import { CorsOptions } from 'cors'
+// //Cors
+// export const corsConfig: CorsOptions = {
+//     origin: function(origin, callback) {
+//         const whitelist = [process.env.FRONTEND_URL]
+
+//         if(process.argv[2] === '--api') {
+//             whitelist.push(undefined)
+//         }
+
+//         if(whitelist.includes(origin)) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Error de CORS'))
+//         }
+//     }
+// }
+
+import { CorsOptions } from 'cors';
+
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
-
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
-
-        if(whitelist.includes(origin)) {
-            callback(null, true)
+    origin: function (origin, callback) {
+        if (origin === process.env.FRONTEND_URL || !origin) {
+            // Permite el acceso si el origen coincide con FRONTEND_URL o si el origen es undefined (para herramientas como Postman)
+            callback(null, true);
         } else {
-            callback(new Error('Error de CORS'))
+            callback(new Error('Error de CORS'));
         }
     }
-}
+};
